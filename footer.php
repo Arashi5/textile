e40a0f2984d5b447e2f28a41270d45a4a044cd54
Alexey37 <73131578+Alexey37@users.noreply.github.com>
2021-03-17 22:16:27 +0300

    test2

--- footer.php
+++ footer.php
@@ -1,27 +1,27 @@
 <?php
 $today = date("d.m.Y H:i:s");
 ?>
-
-    <div class="container-fluid footer-container">
-        <div class="row justify-content-md-center">
-            <div class="col-md-auto">
-                <span class="font-weight">Разработка - Russian Crab Studio (\/)_oO_(\/)</span>
+        <div class="container-fluid footer-container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                    <span class="font-weight">Разработка - Russian Crab Studio </span>
+                    <img width='50' height='50' src='assets/images/crab.png'>
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                    <address>
+                        <span class="sub-text">г. Иваново, ул. Ермака, 49, 153025</span>
+                    </address>
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                    <img width="25" height="25" src='assets/images/ball2.png'>
+                    <span class = "font-weight"><?php echo $today?></span>
+                    <img width="25" height="25" src='assets/images/ball2.png'>
+                </div>
             </div>
         </div>
-        <div class="row justify-content-md-center">
-            <div class="col-md-auto">
-                <address>
-                    <span class="sub-text">г. Иваново, ул. Ермака, 49, 153025</span>
-                </address>
-            </div>
-        </div>
-        <div class="row justify-content-md-center">
-            <div class="col-md-auto">
-                <img width="25" height="25" src="../images/ball2.png">
-                <span class = "font-weight"><?php echo $today?></span>
-                <img width="25" height="25" src="../images/ball2.png">
-            </div>
-        </div>
-    </div>
-</body>
+    </body>
 </html>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInit74cb663372043b6c59159eecbeaefb14
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit74cb663372043b6c59159eecbeaefb14', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit74cb663372043b6c59159eecbeaefb14', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit74cb663372043b6c59159eecbeaefb14::getInitializer($loader));
         } else {
